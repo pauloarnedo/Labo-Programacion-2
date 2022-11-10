a260928f9d789503b560d1453b078138ece47ab6
Paulo Arnedo <dev.pauloarnedo@gmail.com>
2022-11-09 21:42:54 -0300

    Delegados y Expresiones Lambda

---
+++ 2022-11-09/Práctica en clase/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_en_clase
+{
+    //Si al delegado solo lo voy a usar en la clase, lo creo en la clase.
+    //Si necesito usarlo en más lugares lo creo en el namespace, por fuera de la clase.
+    public delegate bool MiDelegadoComparador(int num1, int num2);
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        public Persona(string nombre, int edad)
+        {
+            this.edad = edad;
+            this.nombre = nombre;
+        }
+
+        public string SaludarSinParametros()
+        {
+            return nombre;
+        }
+
+        public string SaludarConDni()
+        {
+            return "Hola mi dni es " + this.edad.ToString();
+        }
+
+        public string SaludarConMensaje(string msg)
+        {
+            return "el mensaje es " + msg;
+        }
+
+        public int FuncionConDelegado(MiDelegadoComparador comparador, List<int> listaEneteros)
+        {
+            //Normalmente los delegados se crean dentro de la clase.
+            int aux = listaEneteros[0];
+            for(int i=0;i < listaEneteros.Count; i++)
+            {
+                if(comparador(aux, listaEneteros[i]))
+                {
+                    aux = listaEneteros[i];
+                }
+            }
+            return aux;
+        }
+    }
+}
---
+++ 2022-11-09/Práctica en clase/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+delegados: cuando le quiero pasar una funcion a una funcion. Como no puedo hacer eso tengo que usar un delegado. Guarda la ref al metodo y a la instancia que lo invocó.
+
+se declara con la palabra reservada delegate.
+tiene firma pero no contenido
+
+a partir de este delegado puedo llamar cualquier otro método que tenga la misma firma
+*/
+
+namespace Práctica_en_clase
+{
+    internal class Program
+    {
+        public delegate string MiDelegado();
+        public delegate void DelegadoSinRetornoNiParametros();
+        public delegate string DelegadoConUnParametro(string mensaje);
+        //public delegate void DelegadoConDosParametros(string nombre, int edad);
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Pedro", 22);
+            Persona p2 = new Persona("Juan", 33);
+            Persona p3 = new Persona("Marcos", 44);
+
+            //instancio el delegado. Le paso el puntero del metodo que voy a usar.
+            MiDelegado delegadito = new MiDelegado(p1.SaludarConDni);
+
+            //llamo al delegado.
+            Console.WriteLine(delegadito());
+            
+            //apunto a otro método.
+            delegadito = p1.SaludarSinParametros;
+            Console.WriteLine(delegadito());
+
+            /**********/
+
+            DelegadoConUnParametro delegadoParametro = p2.SaludarConMensaje;
+            Console.WriteLine(delegadoParametro("Saludos a la flia..."));
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            MiDelegadoComparador delegadoComparador = MiFuncionMayor;
+            List<int> listaEnteros = new List<int>()
+            {
+             5,8,12,99,22,5,1223,54,88,7887,8
+            };
+
+
+            Console.WriteLine( "el num mayor es " + p1.FuncionConDelegado(delegadoComparador, listaEnteros));
+
+            delegadoComparador = MiFuncionMenor;
+            Console.WriteLine("el num menor es " + p1.FuncionConDelegado(delegadoComparador, listaEnteros));
+
+        }
+
+        static bool MiFuncionMayor(int num1, int num2)
+        {
+            if(num1 < num2)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        static bool MiFuncionMenor(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
